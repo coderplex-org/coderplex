dff9d284ada4869283d6ac01041f93331bf6c010
Vinay Puppal <me@vinaypuppal.com>
2017-08-09 16:48:02 +0530

    add back react alias to preact

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ module.exports = {
   webpack: (config, { dev }) => {
     /* Enable only in Production */
     if (!dev) {
-      // // Preact
-      // console.log('> Using Preact instead of React');
-      // config.resolve.alias = {
-      //   react: 'preact-compat/dist/preact-compat',
-      //   'react-dom': 'preact-compat/dist/preact-compat',
-      // };
+      // Preact
+      console.log('> Using Preact instead of React');
+      config.resolve.alias = {
+        react: 'preact-compat/dist/preact-compat',
+        'react-dom': 'preact-compat/dist/preact-compat',
+      };
       if (ANALYZE) {
         const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
         config.plugins.push(
