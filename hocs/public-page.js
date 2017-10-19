ec75b424d5c4d9bacf2e780337b592a3ccc0113a
Vinay Puppal <me@vinaypuppal.com>
2017-10-19 18:40:57 +0530

    Remove user login and profile functionality (#24)
    * integrate now-travis
    
    * remove login, jobs, projects, profile functionality and pages
    
    * remove image load flickering
    
    * use unfeth instead of axios
    
    * remove moment.js and use date-fns
    
    * visually seperate upcoming and past events
    
    * remove projects page
    
    * add travis deploy script
    
    * add export map
    
    * move event fetch logic to componentDidMount for static export
    
    * fix travis error
    
    * fix typo
    
    * allow travis to comment with deployed url after build succeded
    
    * remove commitId to check if it creates comment
    
    * add some debug info for travis logs
    
    * test again if comment works from travis
    
    * test again if comment works from travis
    
    * add missing in_reply_to field for creating comments
    
    * test again if comment works from travis
    
    * test again if comment works from travis
    
    * add coderplex-bot and test finally
    
    * update blog url

--- hocs/public-page.js
+++ hocs/public-page.js
@@ -1,42 +1,16 @@
 import React from 'react';
 
-import redirect from '../utils/redirect';
-import { authenticate } from '../utils/authenticate';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 export default Page => {
   return class PublicPage extends React.Component {
     static async getInitialProps(ctx) {
-      let authData;
       let initialProps = {};
-      const pathName = ctx.req ? ctx.req.url : ctx.pathname;
-      if (ctx.req && ctx.req.user) {
-        authData = ctx.req.user;
-      } else {
-        try {
-          authData = await authenticate(ctx);
-        } catch (err) {
-          console.error(err);
-          if (Page.getInitialProps) {
-            initialProps = await Page.getInitialProps(ctx);
-          }
-          return { ...initialProps };
-        }
-      }
-
-      if (authData) {
-        if (pathName === '/login') {
-          return redirect(ctx, '/profile');
-        }
-      }
       if (Page.getInitialProps) {
         initialProps = await Page.getInitialProps(ctx);
       }
-      if (!authData) {
-        return { ...initialProps };
-      }
-      return { ...authData, ...initialProps };
+      return { ...initialProps };
     }
     render() {
       return (
