89bcd1750835ffdb7d5a5e00cf5218b566f3702a
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-10-25 21:41:20 +0530

    learn page v2 initial push

--- pages/learn.js
+++ pages/learn.js
@@ -1,147 +1,93 @@
 import React from 'react';
-import { Accordion, Icon, List } from 'semantic-ui-react';
+import { Card, Icon, Label, Popup } from 'semantic-ui-react';
 
 import publicPage from '../hocs/public-page';
 import TopBanner from '../components/top-banner';
 
-export default publicPage(() => {
-  return (
-    <div className="root">
-      <TopBanner
-        pageTitle="Learn"
-        pageSubTitle="Open Source Learning Guides to master your favorite technology"
-      />
-      <main>
-        <section>
-          <Accordion fluid styled>
-            <Accordion.Title>
-              <Icon name="dropdown" />
-              Web Development
-            </Accordion.Title>
-            <Accordion.Content>
-              <Accordion>
-                <Accordion.Title>
-                  <Icon name="dropdown" />
-                  Frontend
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List selection verticalAlign="middle">
-                    <List.Item
-                      target="_blank"
-                      href="https://github.com/coderplex/learn/blob/master/web-dev/Frontend/Libraries%20%26%20Frameworks/Learn-Angular.md"
-                    >
-                      <List.Content>
-                        <List.Header>Angular</List.Header>
-                        <List.Description>
-                          Front-end web application framework by Google
-                        </List.Description>
-                      </List.Content>
-                    </List.Item>
-                    <List.Item
-                      target="_blank"
-                      href="https://github.com/coderplex/learn/blob/master/web-dev/Frontend/Libraries%20%26%20Frameworks/Learn-React.md"
-                    >
-                      <List.Content>
-                        <List.Header>React</List.Header>
-                        <List.Description>
-                          JavaScript library for building user interfaces from
-                          Facebook
-                        </List.Description>
-                      </List.Content>
-                    </List.Item>
-                    <List.Item
-                      target="_blank"
-                      href="https://github.com/coderplex/learn/blob/master/web-dev/Frontend/Libraries%20%26%20Frameworks/Learn-VueJS.md"
-                    >
-                      <List.Content>
-                        <List.Header>Vue</List.Header>
-                        <List.Description>
-                          Progressive JavaScript framework for building user
-                          interfaces by community
-                        </List.Description>
-                      </List.Content>
-                    </List.Item>
-                  </List>
-                </Accordion.Content>
-                <Accordion.Title>
-                  <Icon name="dropdown" />
-                  Backend
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List selection verticalAlign="middle">
-                    <List.Item
-                      target="_blank"
-                      href="https://github.com/coderplex/learn/blob/master/web-dev/Backend/Learn-Laravel.md"
-                    >
-                      <List.Content>
-                        <List.Header>Laravel</List.Header>
-                        <List.Description>PHP web framework</List.Description>
-                      </List.Content>
-                    </List.Item>
-                  </List>
-                </Accordion.Content>
-              </Accordion>
-            </Accordion.Content>
-            <Accordion.Title>
-              <Icon name="dropdown" />
-              Data Science
-            </Accordion.Title>
-            <Accordion.Content>
-              <List selection verticalAlign="middle">
-                <List.Item
-                  target="_blank"
-                  href="https://github.com/coderplex/learn/blob/master/data-science/learn-ds.md"
-                >
-                  <List.Content>
-                    <List.Header>Core Data Science</List.Header>
-                    <List.Description>
-                      A field which provides meaningful information from large
-                      amounts of data.
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-              </List>
-            </Accordion.Content>
-            <Accordion.Title>
-              <Icon name="dropdown" />
-              Decentralized Systems
-            </Accordion.Title>
-            <Accordion.Content>
-              <List selection verticalAlign="middle">
-                <List.Item
-                  target="_blank"
-                  href="https://github.com/coderplex/learn/blob/master/Blockchain/learn-blockchain.md"
-                >
-                  <List.Content>
-                    <List.Header>Blockchain</List.Header>
-                    <List.Description>
-                      A distributed database technology
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-              </List>
-            </Accordion.Content>
-          </Accordion>
-        </section>
-      </main>
-      <style jsx>{`
-        .root {
-          background-color: #ffffff;
-        }
-        main {
-          background-color: #ffffff;
-          max-width: 800px;
-          padding-top: 30px;
-          padding-bottom: 30px;
-          padding-left: 30px;
-          padding-right: 30px;
-          min-height: calc(100vh - 70px);
-          margin: 0 auto;
-        }
-        section {
-          margin: 50px 0;
-        }
-      `}</style>
-    </div>
-  );
-});
+import { dataInRootLearn } from '../utils/mock-data';
+
+class Learn extends React.Component {
+  state = {
+    filter: '',
+  };
+
+  render() {
+    return (
+      <div className="root">
+        <TopBanner
+          pageTitle="Learn"
+          pageSubTitle="Open Source Learning Guides to master your favorite technology"
+        />
+        <main>
+          <section>
+            <filterset>{/* <Button /> */}</filterset>
+            <grid>
+              <Card.Group itemsPerRow={3}>
+                {dataInRootLearn.map(subject => (
+                  <Card
+                    raised
+                    key={subject.id}
+                    label={{ as: 'a', corner: 'left', icon: 'heart' }}
+                  >
+                    <Label attached="bottom right">
+                      <Popup
+                        trigger={
+                          <Icon
+                            style={{ marginRight: '0px' }}
+                            name={
+                              subject.learnGuideStatus ? 'checkmark' : 'info'
+                            }
+                          />
+                        }
+                        position="top center"
+                      >
+                        {subject.learnGuideStatus
+                          ? 'Complete guide'
+                          : 'Guide underconstruction'}
+                      </Popup>
+                    </Label>
+                    <logo className={subject.icon} />
+                    <Card.Content>
+                      <Card.Header>{subject.title}</Card.Header>
+                      <Card.Meta>{subject.domain}</Card.Meta>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Icon name="student" />
+                      {`${subject.learningCount} learning`}
+                    </Card.Content>
+                  </Card>
+                ))}
+              </Card.Group>
+            </grid>
+          </section>
+        </main>
+        <style jsx>{`
+          .root {
+            background-color: #ffffff;
+          }
+          main {
+            background-color: #ffffff;
+            max-width: 800px;
+            padding-top: 30px;
+            padding-bottom: 30px;
+            padding-left: 30px;
+            padding-right: 30px;
+            min-height: calc(100vh - 70px);
+            margin: 0 auto;
+          }
+          section {
+            margin: 50px 0;
+          }
+          logo {
+            font-size: 140px;
+            text-align: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default publicPage(Learn);
