aac64967406d99dc7243223f598d2761776edc7e
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-10-31 11:59:32 +0530

    overview section added to learn/

--- pages/learn/subject.js
+++ pages/learn/subject.js
@@ -1,46 +1,130 @@
 import React from 'react';
+import { Tab } from 'semantic-ui-react';
 
-import TopBanner from '../../components/top-banner';
 import publicPage from '../../hocs/public-page';
+import MarkedJS from '../../components/marked-js';
+import { contentsOfLaravel } from '../../utils/mock-data';
 
-export default publicPage(props => (
-  <div>
-    <TopBanner
-      pageTitle={`learn/${props.url.query.id}`}
-      pageSubTitle="Physical spaces for collaboration, peer-learning and self-learning"
-    />
-    <main>
-      <section>
-        <h1>Under construction</h1>
-        <p>You are on learn/{props.url.query.id} page</p>
-      </section>
-    </main>
-    <style jsx>{`
-      main {
-        background-color: #ffffff;
-        padding-top: 30px;
-        padding-bottom: 30px;
-        padding-left: 30px;
-        padding-right: 30px;
-        min-height: calc(100vh - 70px);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        text-align: center;
-        margin: 0 auto;
+export default publicPage(
+  class Subjects extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        overview: '',
+        overviewLoading: true,
+      };
+    }
+
+    componentDidMount() {
+      this.requestOverviewContent();
+    }
+
+    async requestOverviewContent() {
+      try {
+        const overviewPromise = await fetch(contentsOfLaravel.overview);
+        const overview = await overviewPromise.text();
+        await this.setState({
+          overview,
+          overviewLoading: false,
+        });
+      } catch (err) {
+        console.log(err);
       }
-      section {
-        max-width: 800px;
-        margin: 50px 0;
-      }
-      h2 {
-        text-transform: uppercase;
-        padding-bottom: 10px;
-      }
-      divide {
-        width: 100%;
-      }
-    `}</style>
-  </div>
-));
+    }
+
+    render() {
+      return (
+        <div>
+          <header>
+            <logo className="devicon-laravel-plain colored" />
+            <headling>{this.props.url.query.id}</headling>
+          </header>
+          <main>
+            <section>
+              <Tab
+                menu={{
+                  color: 'pink',
+                  attached: false,
+                  tabular: false,
+                  borderless: true,
+                }}
+                panes={[
+                  {
+                    menuItem: 'Overview',
+                    render: () => (
+                      <Tab.Pane attached={false}>
+                        <MarkedJS
+                          loading={this.state.overviewLoading}
+                          markdown={this.state.overview}
+                        />
+                      </Tab.Pane>
+                    ),
+                  },
+                  {
+                    menuItem: 'Guide',
+                    render: () => (
+                      <Tab.Pane attached={false}>Guides will go here</Tab.Pane>
+                    ),
+                  },
+                  {
+                    menuItem: 'Contributors',
+                    render: () => (
+                      <Tab.Pane attached={false}>
+                        {contentsOfLaravel.contributors[0].name}
+                      </Tab.Pane>
+                    ),
+                  },
+                ]}
+              />
+            </section>
+          </main>
+          <style jsx>{`
+            header {
+              display: flex;
+              flex-direction: row;
+              flex-wrap: wrap;
+              justify-content: center;
+              align-content: center;
+              align-items: center;
+              background-color: #f4f7fb;
+              min-height: 200px;
+            }
+            logo {
+              order: 1;
+              flex: 0 1 auto;
+              align-self: auto;
+              font-size: 5em;
+              padding-right: 30px;
+            }
+            headling {
+              order: 2;
+              flex: 0 1 auto;
+              align-self: auto;
+              font-size: 5em;
+              text-align: center;
+            }
+            main {
+              background-color: #ffffff;
+              padding-top: 30px;
+              padding-bottom: 30px;
+              padding-left: 30px;
+              padding-right: 30px;
+              min-height: calc(100vh - 70px);
+            }
+            section {
+              max-width: 800px;
+              margin: 0 auto;
+            }
+            h2 {
+              text-transform: uppercase;
+              padding-bottom: 10px;
+            }
+            divide {
+              width: 100%;
+            }
+          `}</style>
+        </div>
+      );
+    }
+  },
+);
