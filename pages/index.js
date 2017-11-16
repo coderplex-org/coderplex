3d2501a7962bae30b0dc6b4e4940906fb27daccd
Vinay Puppal <me@vinaypuppal.com>
2017-11-16 07:14:50 +0530

    extract subject card to file

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import Hide, { Container, Button, breakpoints } from '../utils/base.styles';
 import { listOfSubjects } from '../utils/mock-data';
 import Layout from '../components/common/layout';
+import SubjectCard from '../components/learn/subject-card';
 
 const HeroSection = styled.section`
   background-color: #fff;
@@ -149,57 +150,6 @@ const SpaceOverlay = styled.div`
   }
 `;
 
-const Subject = styled.a`
-  text-decoration: none;
-  width: calc(33.33% - 40px);
-  margin: 20px;
-  display: inline-block;
-  min-height: 200px;
-  background: #fff;
-  border: 1px solid #b9b9b9;
-  transition: all 0.25s;
-  cursor: pointer;
-  &:hover {
-    transform: translate(0, -2px) scale(1.05);
-  }
-  & .icon {
-    padding: 10px 15px;
-    font-size: 10rem;
-    ${breakpoints.xs} {
-      font-size: 8rem;
-    }
-  }
-  & .content {
-    padding: 10px 0 10px 30px;
-    color: #444;
-    background: #f6f6f6;
-    text-align: left;
-  }
-  & .title {
-    font-size: 1.5rem;
-    font-size: 600;
-    color: #222;
-    ${breakpoints.xs} {
-      font-size: 1.2rem;
-    }
-  }
-  & .subtitle {
-    font-size: 0.8rem;
-    color: #444;
-  }
-  ${breakpoints.md} {
-    width: calc(50% - 40px);
-  }
-  ${breakpoints.sm} {
-    width: calc(50% - 40px);
-    margin: 20px auto;
-  }
-  ${breakpoints.xs} {
-    width: 90%;
-    margin: 20px auto;
-  }
-`;
-
 export default () => (
   <Layout>
     <HeroSection>
@@ -227,19 +177,7 @@ export default () => (
           <Box width={1} pb={[2, 3]} pt={[1]} px={[2, 0]}>
             <Flex justify="center" align="center" wrap>
               {take(listOfSubjects, 6).map(subject => {
-                return (
-                  <Link href={`/learn/subject?id=${subject.subjectId}`} as={subject.url} key={subject.url}>
-                    <Subject href={subject.url}>
-                      <div className="icon">
-                        <i className={subject.icon} />
-                      </div>
-                      <div className="content">
-                        <div className="title">{subject.title}</div>
-                        <div className="subtitle">{subject.domain}</div>
-                      </div>
-                    </Subject>
-                  </Link>
-                );
+                return <SubjectCard key={subject.url} subject={subject} />;
               })}
             </Flex>
           </Box>
