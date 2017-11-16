cfab820065e3cb5642449d92545a4648b282b6b2
Vinay Puppal <me@vinaypuppal.com>
2017-11-16 11:22:43 +0530

    change white button hover color

--- utils/base.styles.js
+++ utils/base.styles.js
@@ -53,7 +53,7 @@ export const Button = styled.a`
   font-size: ${props => (props.large ? '1.8rem' : props.medium ? '1rem' : '1rem')}
   cursor: pointer;
   &:hover {
-    background: ${props => (props.inverted ? '#6f19ed' : '#eee')};
+    background: ${props => (props.inverted ? '#6f19ed' : '#340080')};
     box-shadow: 2px 4px 8px 0 rgba(46, 61, 73, 0.2);
   }
 `;
