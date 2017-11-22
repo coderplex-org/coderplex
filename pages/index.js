08fe634893775885d03101c3b64f1a56a1ff8efa
z <ahmedzubair216@gmail.com>
2017-11-22 15:58:56 +0530

    abstracted urls to another file

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ import Hide, { Container, Button, Title, SubTitle, breakpoints } from '../utils/
 import { listOfSubjects } from '../utils/mock-data';
 import Layout from '../components/common/layout';
 import SubjectCard from '../components/learn/subject-card';
-import { heroPattern, heroBanner } from '../utils/urls';
+import { heroPatternURL, heroBannerURL, spaceCoverURL, eventsCoverURL } from '../utils/urls';
 
 const HeroSection = styled.section`
   ${space};
   background-color: #fff;
   position: relative;
   text-align: center;
-  background-image: url(${heroPattern});
+  background-image: url(${heroPatternURL});
   & h1 {
     font-size: 2.5rem;
     font-weight: 300;
@@ -127,7 +127,7 @@ export default () => (
       <Container>
         <Flex justify="center" align="center" direction="column">
           <Box width={1}>
-            <img src={heroBanner} alt="words" />
+            <img src={heroBannerURL} alt="words" />
             <h1>On a mission to improve the state of tech across India</h1>
           </Box>
         </Flex>
@@ -186,11 +186,7 @@ export default () => (
             </Box>
           </Box>
           <Box order={1} className="box" width={[1, 1, 1 / 2]} pt={4} pb={[0, 1, 4]} px={[3, 4, 3]}>
-            <img
-              className="space__img"
-              alt="sapce__img"
-              src="https://res.cloudinary.com/coderplex/image/upload/c_scale,w_450/v1510788480/website__assets/space.png"
-            />
+            <img className="space__img" alt="sapce__img" src={spaceCoverURL} />
           </Box>
         </Flex>
       </Container>
@@ -201,10 +197,7 @@ export default () => (
         <Flex align="center" justify="center" wrap>
           <Box order={[2, 2, 1]} width={[1, 1, 1 / 2]} px={[4, 4, 3]}>
             <Hide sm xs>
-              <img
-                src="https://res.cloudinary.com/coderplex/image/upload/c_scale,w_348/v1510788480/website__assets/events.png"
-                alt="events__pic"
-              />
+              <img src={eventsCoverURL} alt="events__pic" />
             </Hide>
           </Box>
           <Box order={1} width={[1, 1, 1 / 2]} px={[2, 3]} pt={[3, 4, 0]} pb={[4, 4, 0]}>
@@ -216,10 +209,7 @@ export default () => (
             </SubTitle>
             <Box className="box" width={[1]} pt={[2]} px={[3, 0]}>
               <Hide md lg>
-                <img
-                  src="https://res.cloudinary.com/coderplex/image/upload/c_scale,w_316/v1510788480/website__assets/events.png"
-                  alt="events__pic"
-                />
+                <img src={eventsCoverURL} alt="events__pic" />
               </Hide>
               <Link href={'/events'}>
                 <Button href={'/events'} medium>
