57e11442a552a6b71989ead0e8cea98e41d116a3
Krushi Raj Tula <krushiraj123@gmail.com>
2019-09-05 16:45:19 +0530

    Update feedback.js
    Removed console log.

--- pages/feedback.js
+++ pages/feedback.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Form from '../components/common/airtable-form';
 
-console.log(Form);
-
 export default () => {
   return <Form formId="shrhz9WWHYRzNhJNl" height="2190" />;
 };
